e008fa73de667924dc1d072b81c7d14c28620961
devesh-takoor <92701740+devesh-takoor@users.noreply.github.com>
2022-02-13 22:27:49 +0400

    Add files via upload
    Figuring out a way how to use base64 to upload the images for the categories
    otherwise the other inputs are okay are okay

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, session, redirect, url_for, render_template
+import psycopg2 #pip install psycopg2 
+import psycopg2.extras
+import re 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
+app.secret_key = 'cairocoders-ednalan'
+
+DB_HOST = "localhost"
+DB_NAME = "s10"
+DB_USER = "postgres"
+DB_PASS = "papudirrtw"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -9,19 +21,39 @@ def home():
 
 
 
-@app.route('/signup', methods=['GET'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+   cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   if request.method == 'POST' and 'firstName' and 'lastName' and 'nic' and 'phonenumber' and 'email' and 'password' and 'confirmPassword':
+      firstName = request.form['firstName']
+      lastName = request.form['lastName']
+      nic = request.form['nic']
+      phonenumber = request.form['phonenumber']
+      email = request.form['email']
+      password = request.form['password'] 
+      confirmPassword = request.form['confirmPassword']        
+      cursor.execute("INSERT INTO users (firstname,lastname,nic,phonenumber, email, password,confirmPassword ) VALUES (%s,%s,%s,%s,%s,%s,%s)", (firstName,lastName, nic,phonenumber, email,password,confirmPassword))
+      conn.commit()	
    return render_template('signup.html')
 
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
    return render_template('login.html')
 
 
 @app.route('/c1', methods=['GET'])
 def c1():
+   cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   if request.method == 'POST' and 'files' and 'productname1' and 'productmodel1' and 'productdescription1' and 'productprice1':
+      files = request.files['files']
+      productname1 = request.form['productname1']
+      productmodel1 = request.form['productmodel1']
+      productdescrition1 = request.form['productdescription1']
+      productprice1 = request.form['productprice1']
+      cursor.execute("INSERT INTO category_1 ((title), productname1, productmodel1, productdescription1, productprice1) VALUES (%s,%s,%s,%s,%s)", (files, productname1, productmodel1, productdescription1, productprice1))
+      conn.commit()
    return render_template('c1.html')
 
 
