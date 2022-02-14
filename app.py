fa4ed6bdc1818a2cd6c4d52ea7b71c92dd516b87
devesh-takoor <92701740+devesh-takoor@users.noreply.github.com>
2022-02-15 00:59:21 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, session, redirect, url_for, render_template
+from flask import Flask, request, session, redirect, url_for, render_template, flash
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
 import re 
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from werkzeug.utils import secure_filename
+import os
+UPLOAD_FOLDER = 'static/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.secret_key = 'cairocoders-ednalan'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 DB_HOST = "localhost"
 DB_NAME = "s10"
@@ -14,9 +18,11 @@ DB_USER = "postgres"
 DB_PASS = "papudirrtw"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+mycursor =conn.cursor()
 
 @app.route('/', methods=['GET'])
 def home():
+
    return render_template('home.html')
 
 
@@ -33,29 +39,71 @@ def signup():
       password = request.form['password'] 
       confirmPassword = request.form['confirmPassword']        
       cursor.execute("INSERT INTO users (firstname,lastname,nic,phonenumber, email, password,confirmPassword ) VALUES (%s,%s,%s,%s,%s,%s,%s)", (firstName,lastName, nic,phonenumber, email,password,confirmPassword))
-      conn.commit()	
+      conn.commit()
+      return redirect(url_for('/'))
    return render_template('signup.html')
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+   cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        print(password)
+ 
+        # Check if account exists using MySQL
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+ 
+        if account:
+            password_rs = account['password']
+            print(password_rs)
+            # If account exists in users table in out database
+            if password(password_rs, password):
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['email'] = account['email']
+                # Redirect to home page
+                return redirect(url_for('/'))
+               
    return render_template('login.html')
 
 
-@app.route('/c1', methods=['GET'])
+@app.route('/c1', methods=['GET','POST'])
 def c1():
+   mycursor.execute("SELECT * FROM category_1")
+   data = mycursor.fetchall()
+   print(data)
+   return render_template('c1.html', data=data)
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-   if request.method == 'POST' and 'files' and 'productname1' and 'productmodel1' and 'productdescription1' and 'productprice1':
-      files = request.files['files']
+   if request.method == 'POST':
+      filenames ='';
+      files = request.files.to_dict()
+      print(files)
+      
+      files = request.files.getlist("files[]")
+      for file in files:
+         filename = secure_filename(file.filename)
+         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+         filenames += filename + ';'
+      filenames=filenames[:-1]
       productname1 = request.form['productname1']
       productmodel1 = request.form['productmodel1']
-      productdescrition1 = request.form['productdescription1']
+      productdescription1 = request.form['productdescription1']
       productprice1 = request.form['productprice1']
-      cursor.execute("INSERT INTO category_1 ((title), productname1, productmodel1, productdescription1, productprice1) VALUES (%s,%s,%s,%s,%s)", (files, productname1, productmodel1, productdescription1, productprice1))
+      cursor.execute("INSERT INTO category_1 (productname1, productmodel1, productdescription1, productprice1,filenames) VALUES (%s,%s,%s,%s,%s)", (productname1, productmodel1, productdescription1, productprice1,filenames))
+      
       conn.commit()
-   return render_template('c1.html')
-
+      mycursor.execute("SELECT * FROM category_1")
+      data = mycursor.fetchall()
+   return redirect(url_for('c1'))
 
 
 @app.route('/c2', methods=['GET'])
